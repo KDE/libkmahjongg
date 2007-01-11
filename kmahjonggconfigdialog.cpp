5d381a3bc4f630b8c8a047c1f817339937e6f950
Laurent Montel <montel@kde.org>
2007-01-11 16:39:23 +0000

    kapplication->QApplication (thanks david script :))
    svn path=/trunk/KDE/kdegames/libkmahjongg/; revision=622344

--- kmahjonggconfigdialog.cpp
+++ kmahjonggconfigdialog.cpp
@@ -16,7 +16,6 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include <kapplication.h>
 #include <klocale.h>
 #include <kstandarddirs.h>
 #include <krandom.h>
